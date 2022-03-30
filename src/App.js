7d6ceb1e7014e66119203f1aded3f016b26d22b1
Rendy Proklamanta <rendydwi@myself.com>
2022-03-30 21:53:43 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <p>Hi theres!</p>
+        <p>Hi there!</p>
         <p>We use branch : {BRANCH}</p>
         <p>Node Environment : {NODE_ENV}</p>
 
